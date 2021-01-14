6193bd8e2a1987a056feb1dd01aca57bf10a6b3b
Purity <66000596+graalpurity@users.noreply.github.com>
2021-01-14 13:50:50 -0500

    Add files via upload

---
+++ Handler/Skid/Forms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skid_Panel_Template_V2.Handler.Skid
+{
+    public static class Forms
+    {
+        public static void Home(object sender, EventArgs e)
+        {
+            var Main = new MainFrm();
+            //Main.Panel.Controls.Add(object);
+        }
+
+        public static void ClearPanel()
+        {
+            var Main = new MainFrm();
+            Main.Panel.Controls.Clear();
+        }
+    }
+}
