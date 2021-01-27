79a8124a32246d5d562795f4847234b3049474ad
Purity <66000596+graalpurity@users.noreply.github.com>
2021-01-26 19:18:40 -0500

    Add files via upload

---
+++ Frms/Controls/Settings.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Skid_Panel_Template_V2.Frms.Controls
+{
+    partial class Settings
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topMostCheckBox = new Guna.UI2.WinForms.Guna2CheckBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.restartRpcBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.closeRpcBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SuspendLayout();
+            // 
+            // topMostCheckBox
+            // 
+            this.topMostCheckBox.AutoSize = true;
+            this.topMostCheckBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.topMostCheckBox.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.topMostCheckBox.CheckedState.BorderRadius = 0;
+            this.topMostCheckBox.CheckedState.BorderThickness = 0;
+            this.topMostCheckBox.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.topMostCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.topMostCheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.topMostCheckBox.Location = new System.Drawing.Point(3, 23);
+            this.topMostCheckBox.Name = "topMostCheckBox";
+            this.topMostCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.topMostCheckBox.TabIndex = 0;
+            this.topMostCheckBox.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.topMostCheckBox.UncheckedState.BorderRadius = 0;
+            this.topMostCheckBox.UncheckedState.BorderThickness = 0;
+            this.topMostCheckBox.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.topMostCheckBox.CheckedChanged += new System.EventHandler(this.topMostCheckBox_CheckedChanged);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(51, 17);
+            this.guna2HtmlLabel1.TabIndex = 2;
+            this.guna2HtmlLabel1.Text = "TopMost";
+            // 
+            // restartRpcBtn
+            // 
+            this.restartRpcBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.restartRpcBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.restartRpcBtn.CheckedState.Parent = this.restartRpcBtn;
+            this.restartRpcBtn.CustomImages.Parent = this.restartRpcBtn;
+            this.restartRpcBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.restartRpcBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.restartRpcBtn.ForeColor = System.Drawing.Color.White;
+            this.restartRpcBtn.HoverState.Parent = this.restartRpcBtn;
+            this.restartRpcBtn.Location = new System.Drawing.Point(226, 23);
+            this.restartRpcBtn.Name = "restartRpcBtn";
+            this.restartRpcBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.restartRpcBtn.ShadowDecoration.Parent = this.restartRpcBtn;
+            this.restartRpcBtn.Size = new System.Drawing.Size(15, 14);
+            this.restartRpcBtn.TabIndex = 3;
+            this.restartRpcBtn.Click += new System.EventHandler(this.restartRpcBtn_Click);
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(226, 0);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(70, 17);
+            this.guna2HtmlLabel2.TabIndex = 4;
+            this.guna2HtmlLabel2.Text = "Restart RPC";
+            // 
+            // closeRpcBtn
+            // 
+            this.closeRpcBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.closeRpcBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.closeRpcBtn.CheckedState.Parent = this.closeRpcBtn;
+            this.closeRpcBtn.CustomImages.Parent = this.closeRpcBtn;
+            this.closeRpcBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.closeRpcBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.closeRpcBtn.ForeColor = System.Drawing.Color.White;
+            this.closeRpcBtn.HoverState.Parent = this.closeRpcBtn;
+            this.closeRpcBtn.Location = new System.Drawing.Point(99, 23);
+            this.closeRpcBtn.Name = "closeRpcBtn";
+            this.closeRpcBtn.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.closeRpcBtn.ShadowDecoration.Parent = this.closeRpcBtn;
+            this.closeRpcBtn.Size = new System.Drawing.Size(15, 14);
+            this.closeRpcBtn.TabIndex = 5;
+            this.closeRpcBtn.Click += new System.EventHandler(this.closeRpcBtn_Click);
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(99, 0);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(73, 17);
+            this.guna2HtmlLabel3.TabIndex = 6;
+            this.guna2HtmlLabel3.Text = "Disable RPC";
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.closeRpcBtn);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.restartRpcBtn);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.topMostCheckBox);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.Name = "Settings";
+            this.Size = new System.Drawing.Size(802, 426);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CheckBox topMostCheckBox;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button restartRpcBtn;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2Button closeRpcBtn;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+    }
+}
---
+++ Frms/Controls/Settings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Skid_Panel_Template_V2.Handler;
+using static Skid_Panel_Template_V2.Handler.DiscordH;
+using System.Threading;
+
+namespace Skid_Panel_Template_V2.Frms.Controls
+{
+    public partial class Settings : UserControl
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void topMostCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (topMostCheckBox.Checked)
+            {
+                MainFrm.topMost = true;
+                MainFrm.ActiveForm.TopMost = true;
+            }
+            else
+            {
+                MainFrm.topMost = false;
+                MainFrm.ActiveForm.TopMost = false;
+            }
+        }
+
+        private void closeRpcBtn_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(CheckRPC));
+            if(Discord.Initilized == true)
+            {
+                Discord.Close();
+            }
+            else
+            {
+                MessageBox.Show("Discord is not initilized","Skid Panel Template");
+            }
+        }
+
+        private void restartRpcBtn_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(CheckRPC));
+            if (Discord.Initilized == true)
+            {
+                MessageBox.Show("Discord it already initilized, cannot restart", "Skid Panel Template");
+            }
+            else
+            {
+                Discord.Restart();
+            }
+        }
+
+        public static void CheckRPC()
+        {
+            Discord.Check();
+        }
+    }
+}
